c78868b1cb51b4dcfd3722a23a5bf417319f26c3
4geru <westhouse51@gmail.com>
2024-10-29 00:42:23 +0900

    add table content

--- lib/sg_strange_calendar/table.rb
+++ lib/sg_strange_calendar/table.rb
@@ -1,21 +1,53 @@
 module SgStrangeCalendar
   class Table
+    WEEKDAYS = %w[Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo]
+
     def initialize(year, today = nil)
       @year = year
       @today = today || Date.today
     end
 
     def generate
-      [
-        header(@year),
-
-      ]
+      [ header(@year) ] +
+        ((0..11).to_a).map { |month| Row.new(@year, month, @today).generate }
     end
 
     private
 
     def header(year)
-      [year] + %w[Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo]
+      [year] + WEEKDAYS
+    end
+
+    class Row
+      def initialize(year, month, today = nil)
+        @year = year
+        @month = month
+        @today = today || Date.today
+      end
+
+      def generate
+        date = start_of_month
+        [ start_of_month.strftime('%b') ] +
+          start_blank_days +
+          (start_of_month..end_of_month).to_a +
+          end_blank_days
+      end
+
+      def start_blank_days
+        start_of_month.strftime('%w').to_i.times.map { |_| nil }
+      end
+
+      def end_blank_days
+        (WEEKDAYS.size - start_blank_days.length - end_of_month.day ).times.map { |_| nil }
+      end
+
+      def start_of_month
+        Date.new(@year, @month + 1, 1)
+      end
+
+      def end_of_month
+        Date.new(@year, @month + 1, -1)
+      end
     end
   end
 end
