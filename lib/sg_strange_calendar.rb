ede4c1bfa8cedee550dd75642360017d0263aef5
4geru <westhouse51@gmail.com>
2024-10-29 20:00:46 +0900

    add error message

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -9,6 +9,7 @@ class SgStrangeCalendar
   end
 
   def generate(vertical: false)
+    return invalid_message unless valid?
     # [
     #   %w[year Su Mo Tu We Th Fr ... Sa Su Mo]
     #   [Mon, nil, nil, ... Date1, Date2, ... Date31, nil, nil, ...]]
@@ -27,5 +28,24 @@ class SgStrangeCalendar
         @today
       )
     end
+  end
+
+  private
+
+  def invalid_message
+    year = 1996
+    today = Date.new(1996, 3, 2)
+    table = SgStrangeCalendar::Table.new(year, today).generate
+    "\n==== error === invalid === year ====" \
+      + "\n\n" +  ("🎂" * 40) + "\n\n" \
+      + SgStrangeCalendar::Presenter::HorizonalPresenter.new.present(
+        table,
+        today) \
+      + "\n\n" + ("🎂" * 40) \
+      + "\n\n" + "しげるの誕生日は 1996年3月2日 です！\n"
+  end
+
+  def valid?
+    1000 <= @year && @year <= 9999
   end
 end
