57951ae9306509431784e8f7075215a304da55b4
rhiroe <ride.poke@gmail.com>
2024-10-29 12:36:19 +0000

    refactor

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,36 +1,70 @@
 require 'date'
 
 class SgStrangeCalendar
-  WDAYS = %w[Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo].freeze
-  MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
-  BLANK = ' '.freeze
-
   def initialize(year, today = nil)
     @year = year
     @today = today
   end
 
   def generate(vertical: false)
-    header = (vertical ? MONTHS : WDAYS).dup.unshift("#{@year}")
-    ths = (vertical ? WDAYS : MONTHS).dup
-    length = vertical ? 3 : 2
-    calendar = days(length:).then { vertical ? _1.transpose : _1 }.map do |line|
-      line.unshift(ths.shift.ljust(4))
-      line.join(' ').then { _1.gsub(/\s(\[\d{,2}\])(?:\s|\z)/) { "#{$1}" } }.rstrip
-    end
-    calendar.unshift(header.join(' ')).join("\n")
+    builder = vertical ? VerticalBuilder : HorizontalBuilder
+    builder.new(@year, @today).build
   end
 
-  private
+  class Builder
+    WDAYS = %w[Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo].freeze
+    MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
+    BLANK = ' '.freeze
 
-  def days(length:)
-    [*1..12].map do |month|
+    private_constant :WDAYS, :MONTHS, :BLANK
+
+    def initialize(year, today)
+      @year = year
+      @today = today
+    end
+
+    def build
+      calendar = rows.map.with_index do |line, i|
+        [ths[i].ljust(4), *line].join(' ').gsub(/\s(\[\d+\])(?:\s|\z)/) { $1 }.rstrip
+      end
+      [header.join(' '), *calendar].join("\n")
+    end
+
+    private
+
+    def days_in(month)
       begining_of_month, end_of_month = Date.new(@year, month, 1), Date.new(@year, month, -1)
       array = [*begining_of_month..end_of_month].map do |date|
         date == @today ? "[#{date.day}]".rjust(length + 2) : date.day.to_s.rjust(length)
       end
-      begining_of_month.wday.times { array.unshift(BLANK.rjust(length)) }
-      array.fill(BLANK.rjust(length), array.size..WDAYS.size - 1)
+      forward_blanks = Array.new(begining_of_month.wday, BLANK.rjust(length))
+      array.unshift(*forward_blanks)
+      array.fill(BLANK.rjust(length), array.size...WDAYS.size)
     end
+
+    def header = raise NotImplementedError
+    def ths = raise NotImplementedError
+    def length = raise NotImplementedError
+    def rows = raise NotImplementedError
   end
+
+  class HorizontalBuilder < Builder
+    private
+
+    def header = @header ||= [@year.to_s, *WDAYS.dup]
+    def ths = @ths ||= MONTHS.dup
+    def length = 2
+    def rows = @rows ||= [*1..12].map(&method(:days_in))
+  end
+
+  class VerticalBuilder < Builder
+    private
+
+    def header = @header ||= [@year.to_s, *MONTHS.dup]
+    def ths = @ths ||= WDAYS.dup
+    def length = 3
+    def rows = @rows ||= [*1..12].map(&method(:days_in)).transpose
+  end
+
+  private_constant :Builder, :HorizontalBuilder, :VerticalBuilder
 end
