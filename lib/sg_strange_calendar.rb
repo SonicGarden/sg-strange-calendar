fe72acf6e87f636ae3fed105406a9804ec557eeb
rhiroe <ride.poke@gmail.com>
2024-10-29 02:38:49 +0000

    .

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,9 +1,36 @@
+require 'date'
+
 class SgStrangeCalendar
+  WDAYS = %w[Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo].freeze
+  MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
+  BLANK = ' '.freeze
+
   def initialize(year, today = nil)
-    # write your code here
+    @year = year
+    @today = today
   end
 
   def generate(vertical: false)
-    # write your code here
+    header = (vertical ? MONTHS : WDAYS).dup.unshift("#{@year}")
+    ths = (vertical ? WDAYS : MONTHS).dup
+    length = vertical ? 3 : 2
+    calendar = days(length:).then { vertical ? _1.transpose : _1 }.map do |line|
+      line.unshift(ths.shift.ljust(4))
+      line.join(' ').then { _1.gsub(/\s(\[\d{,2}\])(?:\s|\z)/) { "#{$1}" } }.rstrip
+    end
+    calendar.unshift(header.join(' ')).join("\n")
+  end
+
+  private
+
+  def days(length:)
+    [*1..12].map do |month|
+      begining_of_month, end_of_month = Date.new(@year, month, 1), Date.new(@year, month, -1)
+      array = [*begining_of_month..end_of_month].map do |date|
+        date == @today ? "[#{date.day}]".rjust(length + 2) : date.day.to_s.rjust(length)
+      end
+      begining_of_month.wday.times { array.unshift(BLANK.rjust(length)) }
+      array.fill(BLANK.rjust(length), array.size..WDAYS.size - 1)
+    end
   end
 end
