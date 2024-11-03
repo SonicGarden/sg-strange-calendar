ed485baa84e8059ebc95a2bf3686bf0a63c30316
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-11-03 10:58:42 +0900

    initialize

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,9 +1,29 @@
-class SgStrangeCalendar
+require 'date'
+class SgStrangeCalendar  
   def initialize(year, today = nil)
-    # write your code here
+    @year = year
+    @today = today
+    @calendar = generate_calendar_hash(year)
   end
 
   def generate(vertical: false)
-    # write your code here
+    
+  end
+
+  def generate_calendar_hash(year)
+    result = {}
+
+    Date.new(year, 1, 1).step(Date.new(year, 12, 31)).each do |date|
+      month = date.strftime('%b') # Jan, Feb, ...
+      weekday = date.strftime('%a') # Mo, Tu, ...
+
+      result[month] ||= {}
+      result[month][date.day] = weekday
+    end
+
+    result
   end
 end
+
+c = SgStrangeCalendar.new(2024)
+puts c.generate
