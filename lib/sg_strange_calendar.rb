95d71395eddef83d4a84c9b62b545a6e826ab729
Junichi Ito <jit@sonicgarden.jp>
2024-11-29 09:22:45 +0900

    Clear level 2

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,6 +1,7 @@
 class SgStrangeCalendar
   def initialize(year, today = nil)
     @year = year
+    @today = today
   end
 
   def generate(vertical: false)
@@ -11,9 +12,13 @@ class SgStrangeCalendar
       month = first_date.strftime('%b')
       days = Array.new(first_date.wday, '  ')
       first_date.upto(last_date) do |date|
-        days << date.day.to_s.rjust(2)
+        days.push(date == @today ? "[#{date.day}]" : date.day.to_s.rjust(2))
       end
-      rows << "#{month}  #{days.join(' ')}"
+      row = "#{month}  #{days.join(' ')}"
+              .sub(/(\[\d\]) (\d)/, '\1\2')
+              .sub(/(\d) (\[\d\d\])/, '\1\2')
+              .sub(/(\[\d\])  (\d)/, '\1 \2')
+      rows << row
     end
     rows.join("\n")
   end
