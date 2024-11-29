26990dc68c4bfa0d2bb9cc38255761723578ed44
Junichi Ito <jit@sonicgarden.jp>
2024-11-29 09:32:57 +0900

    Add 2024-01-10 for level 2

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -15,9 +15,9 @@ class SgStrangeCalendar
         days.push(date == @today ? "[#{date.day}]" : date.day.to_s.rjust(2))
       end
       row = "#{month}  #{days.join(' ')}"
-              .sub(/(\[\d\]) (\d)/, '\1\2')
-              .sub(/(\d) (\[\d\d\])/, '\1\2')
-              .sub(/(\[\d\])  (\d)/, '\1 \2')
+              .sub(/(\d\]) (\d)/, '\1\2')   # [10] 11 => [10]11
+              .sub(/(\d\])  (\d)/, '\1 \2') # [1]  2  => [1] 2
+              .sub(/(\d) (\[\d\d)/, '\1\2') # 9 [10]  => 9[10]
       rows << row
     end
     rows.join("\n")
