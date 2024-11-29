01d197224e635bf908ce714abe13946453ff7f3d
Junichi Ito <jit@sonicgarden.jp>
2024-11-29 09:09:02 +0900

    Clear level 1

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,9 +1,20 @@
 class SgStrangeCalendar
   def initialize(year, today = nil)
-    # write your code here
+    @year = year
   end
 
   def generate(vertical: false)
-    # write your code here
+    rows = ["#{@year}#{' Su Mo Tu We Th Fr Sa' * 5} Su Mo"]
+    1.upto(12) do |m|
+      first_date = Date.new(@year, m, 1)
+      last_date = Date.new(@year, m, -1)
+      month = first_date.strftime('%b')
+      days = Array.new(first_date.wday, '  ')
+      first_date.upto(last_date) do |date|
+        days << date.day.to_s.rjust(2)
+      end
+      rows << "#{month}  #{days.join(' ')}"
+    end
+    rows.join("\n")
   end
 end
