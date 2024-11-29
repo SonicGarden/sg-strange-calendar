6921e0a2ea81a097bd5dbdca1749c8614faa9716
Junichi Ito <jit@sonicgarden.jp>
2024-11-29 18:52:05 +0900

    Refactoring

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -39,16 +39,24 @@ class SgStrangeCalendar
 
     vertical_dates.map.with_index do |(wday, *dates), i|
       if i.zero?
-        months = dates.map { |date| date.strftime('%b') }.join(' ')
-        [wday, *months].join(' ')
+        build_header(wday, dates)
       else
-        days = dates.map do |date|
-          day = @today && date == @today ? "[#{date.day}" : date&.day
-          day.to_s.rjust(3)
-        end
-        row = ["#{wday}  ", *days].join(' ')
-        row.sub(/(\[\d+) ?/, '\1]').rstrip
+        build_vertical_body(wday, dates)
       end
     end.join("\n")
+  end
+
+  def build_header(wday, dates)
+    months = dates.map { |date| date.strftime('%b') }.join(' ')
+    [wday, *months].join(' ')
+  end
+
+  def build_vertical_body(wday, dates)
+    days = dates.map do |date|
+      day = @today && date == @today ? "[#{date.day}" : date&.day
+      day.to_s.rjust(3)
+    end
+    row = ["#{wday}  ", *days].join(' ')
+    row.sub(/(\[\d+) ?/, '\1]').rstrip
   end
 end
