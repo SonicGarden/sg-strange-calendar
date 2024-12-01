38d7782e548db56c67e3cfda3472d172989acdb5
Junichi Ito <jit@sonicgarden.jp>
2024-12-01 21:45:11 +0900

    Unuse OOP

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -1,74 +1,50 @@
 class SgStrangeCalendar
+  FIRST_COL_WIDTH = 4
+  MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
+  WDAYS = %w[Su Mo Tu We Th Fr Sa].cycle.take(37)
+
   def initialize(year, today = nil)
-    @horizontal = HorizontalGenerator.new(year, today)
-    @vertical = VerticalGenerator.new(year, today)
+    @year = year
+    @today = today
   end
 
   def generate(vertical: false)
-    generator = vertical ? @vertical : @horizontal
-    generator.generate_calendar
+    data = generate_data
+    data = transpose_data(*data) if vertical
+    date_width = vertical ? 4 : 3
+    data.map.with_index do |(first_col, *values), i|
+      i.zero? ? build_header_row(first_col, values) : build_body_row(first_col, values, date_width)
+    end.join("\n")
   end
 
-  class Generator
-    FIRST_COL_WIDTH = 4
-    MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
-    WDAYS = %w[Su Mo Tu We Th Fr Sa].cycle.take(37)
+  private
 
-    def initialize(year, today)
-      @year = year
-      @today = today
+  def transpose_data(wdays, *dates_by_month) = wdays.zip(*dates_by_month)
+
+  def generate_data
+    dates_by_month = 1.upto(12).map do |m|
+      first_date = Date.new(@year, m, 1)
+      last_date = Date.new(@year, m, -1)
+      blank_days = Array.new(first_date.wday)
+      [MONTHS[m - 1], *blank_days, *first_date..last_date]
     end
-
-    def generate_calendar
-      generate_data.map.with_index do |(first_col, *values), i|
-        i.zero? ? build_header_row(first_col, values) : build_body_row(first_col, values)
-      end.join("\n")
-    end
-
-    private
-
-    def generate_data
-      dates_by_month = 1.upto(12).map do |m|
-        first_date = Date.new(@year, m, 1)
-        last_date = Date.new(@year, m, -1)
-        blank_days = Array.new(first_date.wday)
-        [MONTHS[m - 1], *blank_days, *first_date..last_date]
-      end
-      [[@year, *WDAYS]] + dates_by_month
-    end
-
-    def build_header_row(year, values)
-      [year, *values].join(' ')
-    end
-
-    def build_body_row(first_col, dates)
-      days = dates.map do |date|
-        day = add_left_bracket?(date) ? "[#{date.day}" : date&.day
-        day.to_s.rjust(date_width)
-      end
-      row = [first_col.ljust(FIRST_COL_WIDTH), *days].join
-      insert_right_bracket(row).rstrip
-    end
-
-    def add_left_bracket?(date) = @today && date == @today
-
-    def insert_right_bracket(row) = row.sub(/(\[\d+) ?/, '\1]')
+    [[@year, *WDAYS]] + dates_by_month
   end
 
-  class HorizontalGenerator < Generator
-    private
-
-    def date_width = 3
+  def build_header_row(year, values)
+    [year, *values].join(' ')
   end
 
-  class VerticalGenerator < Generator
-    private
-
-    def generate_data
-      wdays, *dates_by_month = super
-      wdays.zip(*dates_by_month)
+  def build_body_row(first_col, dates, date_width)
+    days = dates.map do |date|
+      day = add_left_bracket?(date) ? "[#{date.day}" : date&.day
+      day.to_s.rjust(date_width)
     end
-
-    def date_width = 4
+    row = [first_col.ljust(FIRST_COL_WIDTH), *days].join
+    insert_right_bracket(row).rstrip
   end
+
+  def add_left_bracket?(date) = @today && date == @today
+
+  def insert_right_bracket(row) = row.sub(/(\[\d+) ?/, '\1]')
 end
