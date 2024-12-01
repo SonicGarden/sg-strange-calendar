64c74d29c27e56d69eff83c675cb87a778d63dff
Junichi Ito <jit@sonicgarden.jp>
2024-12-02 06:48:16 +0900

    Refactoring

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -8,12 +8,9 @@ class SgStrangeCalendar
   end
 
   def generate(vertical: false)
-    header, *body = generate_table(vertical)
+    header, *body_dates = generate_table(vertical)
     header_row = header.join(' ')
-    day_width = vertical ? 4 : 3
-    body_rows = body.map do |first_col, *dates|
-      build_body_row(first_col, dates, day_width)
-    end
+    body_rows = build_body_rows(body_dates, vertical)
     [header_row, *body_rows].join("\n")
   end
 
@@ -31,6 +28,13 @@ class SgStrangeCalendar
     vertical ? wdays.zip(*dates_by_month) : [wdays, *dates_by_month]
   end
 
+  def build_body_rows(body_dates, vertical)
+    day_width = vertical ? 4 : 3
+    body_dates.map do |first_col, *dates|
+      build_body_row(first_col, dates, day_width)
+    end
+  end
+
   def build_body_row(first_col, dates, day_width)
     days = dates.map do |date|
       day = add_left_bracket?(date) ? "[#{date.day}" : date&.day
@@ -40,7 +44,11 @@ class SgStrangeCalendar
     insert_right_bracket(row).rstrip
   end
 
-  def add_left_bracket?(date) = @today && date == @today
+  def add_left_bracket?(date)
+    @today && date == @today
+  end
 
-  def insert_right_bracket(row) = row.sub(/(\[\d+) ?/, '\1]')
+  def insert_right_bracket(row)
+    row.sub(/(\[\d+) ?/, '\1]')
+  end
 end
