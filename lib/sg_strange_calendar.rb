67a9dd18fc5d0a11ed9b0816e6375478a9ed9b62
hashimoto-kei <hashimoto.keigo88@gmail.com>
2024-12-09 05:45:49 +0900

    Refactoring

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -9,12 +9,13 @@ class SgStrangeCalendar
     lines = []
     lines << "#{@year} Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo"
     (1..12).each do |month|
-      month_name = MONTH_NAMES[month - 1]
+      line = []
+      line << MONTH_NAMES[month - 1] + " "
       first_day = Date.new(@year, month, 1)
-      space = "   " * (first_day.cwday % 7)
+      (first_day.cwday % 7).times {line << "  "}
       last_day = Date.new(@year, month, -1)
-      days = (1..last_day.day).map {|day| day.to_s.rjust(2)}.join(" ")
-      lines << month_name + "  " + space + days
+      (1..last_day.day).each {|day| line << day.to_s.rjust(2)}
+      lines << line.join(" ")
     end
     lines.join("\n")
   end
