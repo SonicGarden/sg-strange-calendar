221adecae7c084b81b2a452b65a9496d911adfa4
AkitoShiga <akitoshiga0303@gmail.com>
2024-12-11 00:09:45 +0900

    リファクタ

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -9,9 +9,9 @@ class SgStrangeCalendar
   WEEKDAYS_INDEX = 0
   YEAR_SPACE_SIZE = 1
   WEEKDAYS_SPACE_SIZE = 37
+  WEEKDAYS = %w[Su Mo Tu We Th Fr Sa]
   # 以下のハッシュのキーはDateのAPI仕様に依存している
   MONTHS = (1..12).zip(%w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]).to_h # Date#month
-  WEEKDAYS = (0..6).zip(%w[Su Mo Tu We Th Fr Sa]).to_h # Date#wday
 
   private_constant(*%i[
      MONTHS_INDEX
@@ -41,23 +41,13 @@ class SgStrangeCalendar
   private
 
   def build_outline_filled_calendar
-    calendar = Array.new(YEAR_SPACE_SIZE + MONTHS.size).map do
-      Array.new(YEAR_SPACE_SIZE + WEEKDAYS_SPACE_SIZE, '')
+    calendar = Array.new(YEAR_SPACE_SIZE + WEEKDAYS_SPACE_SIZE).map do
+      Array.new(YEAR_SPACE_SIZE + MONTHS.size, '')
     end
-
-    calendar[0][0] = year.to_s
-
-    MONTHS.each_key do |month_number|
-      calendar[month_number][MONTHS_INDEX] = MONTHS[month_number]
-    end
-
-    WEEKDAYS_SPACE_SIZE.times do |fill_time|
-      week_number = fill_time % WEEKDAYS.size
-      weekday_index = fill_time + YEAR_SPACE_SIZE
-      calendar[WEEKDAYS_INDEX][weekday_index] = WEEKDAYS[week_number]
-    end
-
-    calendar
+    calendar[0] = [nil, *MONTHS.values]
+    transpose_calendar = calendar.transpose
+    transpose_calendar[0] = [year.to_s] + WEEKDAYS.cycle.take(WEEKDAYS_SPACE_SIZE)
+    transpose_calendar
   end
 
   def build_day_filled_calendar(calendar)
@@ -84,21 +74,24 @@ class SgStrangeCalendar
     directed_calendar = vertical ? calendar.transpose : calendar
 
     # 一行目の項目のサイズに依存する
-    cell_size = (vertical ? MONTHS : WEEKDAYS).values.max_by(&:size).size
+    cell_size = (vertical ? MONTHS.values : WEEKDAYS).max_by(&:size).size
     year_size = year.to_s.size
+
     cell_adjusted_calendar = directed_calendar.map do |row|
-      row.map.each_with_index do |value, i|
-          i.zero? ? value.ljust(year_size) : value.rjust(cell_size)
-      end
+      row[0] = row[0].ljust(year_size)
+      row.map.with_index(1) do |value|
+          value.rjust(cell_size)
+      end.join(" ").rstrip
+    end.join("\n")
+
+    if today
+      adjust_emphasized(cell_adjusted_calendar, vertical)
+    else
+      cell_adjusted_calendar
     end
-
-    # todayが指定された際に、[]の分増えたスペースを調整
-    c = cell_adjusted_calendar.map{ |row| row.join(" ").rstrip }
-                          .join("\n")
-    adjust_emphasized(c, vertical)
-
   end
 
+  # todayが指定された際に、[]の分増えたスペースを調整
   def adjust_emphasized(row, vertical)
     common = row.sub(/((?<=\[\d{2}\]) {1})/, '')
 
